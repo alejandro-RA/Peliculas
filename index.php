a3985c310df8a2286046ca33351f56ac2af0b485
alejandro-RA <60902928+alejandro-RA@users.noreply.github.com>
2020-02-10 23:25:17 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        
+        <title>inicio</title>
+        <link rel="stylesheet"  href="./estilos.css">
+
+    </head>
+    <body>
+        <center>
+        
+        <div class="inicio">
+                <form  method="POST" >
+                <marquee><h1> bienvenidos por putas</h1></marquee>
+                      <tr>  
+                        <td><a href="invitado.html">
+                          <input type="button" name="btn_invitado" value="invitado" ></td>
+                        </a>
+
+                        <td><a href="logadm.html">
+                                <input type="button" name="btn_enviar" value="Admin"> 
+                              </a></td>
+
+                        <td><a href="logclient.html">
+                          <input type="button" name="btn_cliente" value="cliente" />
+                        </a></td>
+                      </tr>
+                    </table>
+                </form>
+             </div>
+            </center>
+    </body>
